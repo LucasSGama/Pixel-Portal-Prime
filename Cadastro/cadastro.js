805351c312eb9d957df2f0412560f54c41fd58c7
Lucas-Da-Gama <130692787+Lucas-Da-Gama@users.noreply.github.com>
2023-12-09 19:01:23 -0300

    Tela de cadastro

---
+++ Cadastro/cadastro.js
@@ -0,0 +1,50 @@
+    // FUNÇÃO PARA MOSTRAR A SENHA
+    function mostrarSenha(id) {
+        const input = document.getElementById(id);
+        const icon = document.querySelector(`#${id} + .show-password i`);
+  
+        if (input.type === 'password') {
+          input.type = 'text';
+          icon.classList.remove('bi-eye-slash');
+          icon.classList.add('bi-eye');
+        } else {
+          input.type = 'password';
+          icon.classList.remove('bi-eye');
+          icon.classList.add('bi-eye-slash');
+        }
+      }
+  
+      // FUNÇÃO PARA VALIDAR A SENHA NO INPUT CONFIRME A SENHA
+      let senha = document.getElementById('senha');
+      let confirmaSenha = document.getElementById('confirmaSenha');
+  
+  function validarSenha() {
+    if (senha.value != confirmaSenha.value) {
+      confirmaSenha.setCustomValidity("Senhas diferentes!");
+      confirmaSenha.reportValidity();
+      return false;
+    } else {
+      confirmaSenha.setCustomValidity("");
+      return true;
+    }
+  }
+  
+  // verificar também quando o campo for modificado, para que a mensagem suma quando as senhas forem iguais
+  confirmaSenha.addEventListener('input', validarSenha);
+  
+  // VALIDAR O EMAIL
+      let email = document.getElementById('email');
+      let ConfirmarEmail = document.getElementById('ConfirmarEmail');
+  
+  function validarEmail() {
+    if (email.value != ConfirmarEmail.value) {
+      ConfirmarEmail.setCustomValidity("Emails diferentes!");
+      ConfirmarEmail.reportValidity();
+      return false;
+    } else {
+      ConfirmarEmail.setCustomValidity("");
+      return true;
+    }
+  }
+  
+ 
--- Cadastro/cadastro.php
+++ Cadastro/cadastro.php
@@ -1,44 +1,86 @@
+<?php
+
+    if(isset($_POST['submit']))
+    {
+        include_once('../Base/conexao.php');
+        
+        $nome_de_usuario = $_POST['nome_de_usuario'];
+        $email = $_POST['email'];
+        $senha = $_POST['senha'];
+                    
+        $result = mysqli_query($mysqli, "INSERT INTO usuarios(nome_de_usuario,email,senha)
+        VALUES ('$nome_de_usuario', '$email', '$senha')");
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- BootStrap -->
+    <!-- BootStrap - CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <!-- BootStrap - Icon -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.min.css">
     <!-- Style.css -->
     <link rel="stylesheet" href="cadastro.css">
     <link rel="shortcut icon" href="imgs/icon.png" type="image/x-icon">
     <title>Cadastro</title>
 </head>
 <body>
+    <form novalidate action="cadastro.php" method="POST">
     <div class="content">
         <section>
         <div class="Cadastro">
           <img src="../Imagens-não-oficiais/logo.png" alt="">
             <h2>Cadastro</h2>
             <br>
+            <!-- NOME -->
             <div class="inputBox">
-                <input type="text" placeholder="Nome do Usuário">
+                <input type="text" placeholder="Nome do Usuário" minlength="6" maxlength="18" name="nome_de_usuario" id="nome_de_usuario" required>
+                <p class="subtitulo">Entre 6 e 18 caracteres</p>
+            </div>
+            <!-- EMAIL -->
+            <div class="inputBox">
+              <input type="email" id="email" name="email" placeholder="Email" required>
+            </div>
+            <!-- CONFIMAR O EMAIL -->
+            <div class="inputBox">
+              <input type="email" id="ConfirmarEmail" placeholder="Confirme o email" required>
+            </div>
+            <!-- SENHA -->
+            <div class="inputBox" style="color: gray;">
+                <input id="senha" name="senha" type="password" placeholder="Senha" minlength="8" maxlength="20">
+                <span class="show-password olho1" onclick="mostrarSenha('senha')" required>
+                    <i class="bi bi-eye-slash"></i>
+                </span>
+                <p class="subtitulo">Entre 8 e 20 caracteres</p>
+            </div>
+            <!-- CONFIRMAR SENHA -->
+            <div class="inputBox" style="color: gray;">
+                <input id="confirmaSenha" type="password" placeholder="Confirme a senha"  minlength="8" maxlength="20">
+                <span class="show-password olho2" onclick="mostrarSenha('confirmaSenha')" required>
+                    <i class="bi bi-eye-slash"></i>
+                </span>
             </div>
             <div class="inputBox">
-              <input type="text" placeholder="Email">
-          </div>
-            <div class="inputBox">
-                <input type="password" placeholder="Senha">
-            </div>
-            <div class="inputBox">
-                <input type="submit" value="Cadastrar" id="btn">
+                <a href="../Login/login.html"><input type="submit" value="Enviar" id="submit" name="submit" onclick="return validarEmail()" onclick="return validarSenha()" class="btn"></a>
             </div>
             <div class="group">
-                <a href="#">Já tenho uma conta!</a>
-
+                <a href="../Login/login.html">
+                Depois de enviar clique aqui!
+                <br>    
+                Já tenho uma conta!
+                </a>
             </div>
             <br>
         </div>
     </section>
     </div>
-
+    </form>
     <!-- BootStrap - Script -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXDwqOz6Ff40Cp3aU/ap1ZPKBObCg1WZhSuH" crossorigin="anonymous"></script>
+    <script src="cadastro.js"></script>
 </body>
 </html>
